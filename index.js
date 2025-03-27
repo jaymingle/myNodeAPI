9ec6bfffce13b7e89a7fe4616f7b4a5e99b908ef
Jay Mingle <jkawudi@gmail.com>
2025-03-27 11:09:31 +0000

    Changes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import 'dotenv/config';
 import express from 'express'
 import connectToDB from "./connectDB.js";
+import School from "./models/school.model.js";
+
 const app = express();
 
 const port = process.env.PORT || 6060
 
 console.clear()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 connectToDB('mongodb+srv://jaysubscribes123:j0X6GilyeHYaLZQo@backenddb.ow7zef3.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
 
@@ -15,9 +20,11 @@ app.get('/', (req, res) => {
 })
 
 //Create
-app.get('/products/create_product', async (req, res) => {
+app.post('/products/create_product', async (req, res) => {
     try{
 
+        const school = await School.create(req.body)
+        res.status(200).json(school)
 
     }catch(error){
         res.status(500).json({message: error.message})
@@ -25,7 +32,14 @@ app.get('/products/create_product', async (req, res) => {
 })
 
 //Read All
-
+app.get('/products', async (req, res) => {
+    try{
+        const school = await School.find({})
+        res.status(200).json(school)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
 
 //Read One
 
--- txt.md
+++ txt.md
@@ -1,3 +1,3 @@
 username: jaysubscribes123
 password: j0X6GilyeHYaLZQo
-
+0555250687 - bernice oforiwaaa
