9e493c200b53a7d6b4e5fd10cd5ec1b76e07af6d
Jay Mingle <jkawudi@gmail.com>
2025-03-27 10:10:41 +0000

    Changes

--- models/school.model.js
+++ models/school.model.js
@@ -3,14 +3,28 @@ import mongoose from 'mongoose';
 const ProductSchema = mongoose.Schema(
     {
         name: {
-
+                type: String,
+                required: [true, 'Name is required!!!']
         },
+
         location: {
-
+                type: String,
+                required: false
         },
+
         image: {
-
+                type: String,
+                required: false
         },
 
-}
+            founded: {
+                type: Number,
+                    required: true,
+                    default: 0
+            }
+
+},
+    {
+            timestamps: true
+    }
 )
