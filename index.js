926a5014a745b98cb06daa238f1f2f60d07b7640
Jay Mingle <jkawudi@gmail.com>
2025-03-27 09:16:30 +0000

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('ghana')
+import 'dotenv/config';
+import express from 'express'
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Ghana')
+})
+
+
+app.listen(port, () => console.log('Server is running'))
